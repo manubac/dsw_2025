3d37d8ba5d435e40a12215e720d964fd2cc454f8
manubac <manubacolla@hotmail.com>
2025-09-19 16:39:32 -0300

    app.ts inicial para la base de datos

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,18 @@
+import 'reflect-metadata'
 import express, {NextFunction, Request, Response} from 'express' 
 import { vendedorRouter } from './vendedor/vendedor.routes.js';
 import { cartaRouter } from './carta/carta.routes.js'
+import { orm, syncSchema } from './shared/db/orm.js'
+import { RequestContext } from '@mikro-orm/core';
 
 const app = express()
 app.use(express.json())
+
+//desps de los middleware base
+app.use((req, res, next)=> {
+  RequestContext.create(orm.em, next)
+})
+//antes de las rutas
 
 app.use('/api/vendedores', vendedorRouter);
 app.use('/api/cartas', cartaRouter)
@@ -11,6 +20,10 @@ app.use('/api/cartas', cartaRouter)
 app.use((req, res)=> {
     res.status(404).send({message:'Ruta no encontrada'});
 })
+
+await syncSchema()
+
+//middleware de gestion de errores
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
